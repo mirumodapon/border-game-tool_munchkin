ce53c7b92a8a67a2f65896f6fc3c6f2729656a34
咪路 <anthony.a3203@gmail.com>
2020-10-29 00:37:42 +0800

    Initial server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+// API
+
+app.use('/', require('./router/api/createNewRoom'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
