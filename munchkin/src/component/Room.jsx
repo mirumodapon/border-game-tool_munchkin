258d0f3a4580e1e126b53e44b76221964faee605
咪路 <anthony.a3203@gmail.com>
2020-10-30 11:04:24 +0800

    edit name in room

--- munchkin/src/component/Room.jsx
+++ munchkin/src/component/Room.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import attackIcon from '../image/attack.png';
 import goal from '../image/goal.png';
 import '../css/room.scss';
@@ -8,7 +9,21 @@ class Room extends Component {
             { name: 'mirumo', attack: 20, level: 9 },
             { name: 'hank', attack: 15, level: 4 },
             { name: 'anthony', attack: 6, level: 8 }
-        ]
+        ],
+        name: this.props.name,
+        editName: <span>{this.props.name}&emsp;</span>
+    }
+    completee = () => {
+        let temp = <span>{this.state.name}&emsp;</span>;
+        this.setState({ editName: temp });
+        /// send message
+    }
+    editName = () => {
+        let temp = <input className="editName" onBlur={this.completee} onChange={this.change}></input>;
+        this.setState({ editName: temp });
+    }
+    change = (e) => {
+        this.setState({ name: e.target.value });
     }
     render() {
         const temp = this.state.list.map(
@@ -22,8 +37,8 @@ class Room extends Component {
                 </li>
         );
         const self = (
-            <li className="listItem">
-                <span>{this.props.name}&emsp;</span>
+            <li className="listItem" onClick={this.editName}>
+                {this.state.editName}
                 <img src={goal} className="icon" alt="goal levelIcon" />
                 <span>{this.props.level}&emsp;</span>
                 <img src={attackIcon} className="icon" alt="attackIcon sword" />
@@ -31,10 +46,14 @@ class Room extends Component {
             </li>
         );
         return (
-            <ul>
-                {self}
-                {temp}
-            </ul>
+            <Fragment>
+                <ul>
+                    {self}
+                    {temp}
+                </ul>
+                <br />
+                <button className="exit"><Link to="/">Exit</Link></button>
+            </Fragment>
         );
     }
 }
--- munchkin/src/component/Start.jsx
+++ munchkin/src/component/Start.jsx
@@ -1,11 +1,13 @@
 import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import '../css/start.scss';
 class Start extends Component {
     state = {
         roomNumber: ""
     }
     start = () => {
-
+        console.log('start');
+        // send message
     }
     onChange = (e) => {
         let room = e.target.value;
@@ -18,7 +20,7 @@ class Start extends Component {
                     <div className="center">
                         <span className="text">Room Number</span>
                         <input type="number" className="input" onChange={this.onChange} />
-                        <button className="btn" onClick={this.start}>Start</button>
+                        <button className="btn" onClick={this.start}><Link to="/room">Start</Link></button>
                     </div>
                 </div>
             </Fragment>
