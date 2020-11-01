e8e8e0f65277d2369996c42adf18804ab4526fe3
咪路 <anthony.a3203@gmail.com>
2020-11-01 21:50:12 +0800

    rebuild websocket

--- munchkin/src/component/Room.jsx
+++ munchkin/src/component/Room.jsx
@@ -11,30 +11,6 @@ function Room() {
     const select = useSelector(
         state => state.self
     );
-    var ws = null;
-    useEffect(
-        () => {
-            ws = new WebSocket(`${window.location.origin.replace(/^https/, 'ws').replace(/^http/, 'ws')}/munchkin?room=${select.room}`);
-
-            ws.onopen = () => {
-                let msg = { name: select.name, attack: select.attack, level: select.level, disconnect: false };
-                ws.send('ask');
-                ws.send(JSON.stringify(msg));
-            }
-            ws.onmessage = (msg) => {
-                if (msg.data == "ask") {
-                    let msg = { name: select.name, attack: select.attack, level: select.level, disconnect: false };
-                    ws.send(JSON.stringify(msg));
-                } else disp(update(JSON.parse(msg.data)));
-            }
-            return () => {
-                ws.send(JSON.stringify({ name: select.name, disconnect: true }));
-                ws.close();
-                ws = null;
-            }
-        },
-        [select.name, select.level, select.attack]
-    );
     const self = (
         <li className="listItem">
             <span>{select.name}&emsp;</span>
@@ -65,31 +41,5 @@ function Room() {
         </Fragment>
     );
 }
-
-
-// class Room extends Component {
-//     state = {
-//         list: [
-//             { name: 'mirumo', attack: 20, level: 9 },
-//             { name: 'hank', attack: 15, level: 4 },
-//             { name: 'anthony', attack: 6, level: 8 }
-//         ],
-//         name: this.props.name,
-//         editName: <span>{this.props.name}&emsp;</span>
-//     }
-//     
-//     editName = () => {
-//         let temp = <input className="editName" onBlur={this.completee} onChange={this.change}></input>;
-//         this.setState({ editName: temp });
-//     }
-//     change = (e) => {
-//         this.setState({ name: e.target.value });
-//     }
-//     render() {
-//         
-
-
-//     }
-// }
 
 export default Room;
